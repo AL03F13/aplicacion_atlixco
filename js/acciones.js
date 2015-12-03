71b0590bb92023071549835f7ab8f9b58499ab30
AL03F13 <aponteeric95@gmail.com>
2015-12-03 16:12:36 -0600

    cinco

--- js/acciones.js
+++ js/acciones.js
@@ -5,6 +5,7 @@ document.addEventListener("deviceready",function(){
 		$.ajax({
 			type: "POST",
 			url: "http://192.168.1.195/proyecto_atlixco/buscar_todas.php",
+			//data: "nombre=" . $('#txtnombre').val(),
 			error: function(){
 			 alert("no se puede conectar al servidor");	
 			},
@@ -26,7 +27,33 @@ document.addEventListener("deviceready",function(){
 			    
 			}
 		});
-	});
+	});//tap boton listado completo
+	
+	$('#btn_buscar').on('tap',function(){
+		$.ajax({
+			type: "POST",
+			url: "http://192.168.1.195/proyecto_atlixco/buscar_todas.php",
+			data: "nombre=" . $('#txtnombre').val(),
+			error: function(){
+			 alert("no se puede conectar al servidor");	
+			},
+			success: function(respuesta)
+			{
+				var datos_t = JSON.parse(respuesta);
+				$('#busquedax').empty();
+				
+				
+				
+				for (var $x=0; $x<datos_t.tradiciones.length;$x++)
+				{
+				 	$('#busquedax').append('<div align="center"><div><h4>'+ datos_t.tradiciones[$x].nombre +'</h4></div><div><h4>'+ datos_t.tradiciones[$x].fecha +'</h4></div><div><h4>'+ datos_t.tradiciones[$x].ubicacion +'</h4></div><div><h4>'+ datos_t.tradiciones[$x].descripcion +'</h4></div><div><img src="http://192.168.1.195/proyecto_atlixco/recursos/imagenes/fotos/'+ datos_t.tradiciones[$x].id_evento +'.jpg"></div></div>');
+				}
+				
+				
+			    
+			}
+		});
+	});//tap boton listado completo
 }); 
 });
 
