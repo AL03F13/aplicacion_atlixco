f882eed6f5545682a85e201dc468dba6137299ab
AL03F13 <aponteeric95@gmail.com>
2015-12-03 15:50:50 -0600

    cuatro

--- js/acciones.js
+++ js/acciones.js
@@ -10,7 +10,20 @@ document.addEventListener("deviceready",function(){
 			},
 			success: function(respuesta)
 			{
-				alert(respuesta);
+				var datos_t = JSON.parse(respuesta);
+				$('#contenido').empty();
+				
+				$(':mobile-pagecontainer').pagecontainer('change', '#lista_tradiciones',{
+					transition: 'pop'
+				});
+				
+				for (var $x=0; $x<datos_t.tradiciones.length;$x++)
+				{
+				 	$('#contenido').append('<div align="center"><div><h4>'+ datos_t.tradiciones[$x].nombre +'</h4></div><div><h4>'+ datos_t.tradiciones[$x].fecha +'</h4></div><div><h4>'+ datos_t.tradiciones[$x].ubicacion +'</h4></div><div><h4>'+ datos_t.tradiciones[$x].descripcion +'</h4></div><div><img src="http://192.168.1.195/proyecto_atlixco/recursos/imagenes/fotos/'+ datos_t.tradiciones[$x].id_evento +'.jpg"></div></div>');
+				}
+				
+				
+			    
 			}
 		});
 	});
