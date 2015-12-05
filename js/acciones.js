edb4109689944521329a282cd8966a5b4e6687bc
AL03F13 <aponteeric95@gmail.com>
2015-12-05 08:22:30 -0600

    siete

--- js/acciones.js
+++ js/acciones.js
@@ -4,7 +4,7 @@ document.addEventListener("deviceready",function(){
 	$('#btn_listar').on('tap',function(){
 		$.ajax({
 			type: "POST",
-			url: "http://192.168.1.64/proyecto_atlixco/buscar_todas.php",
+			url: "http://192.168.1.195/proyecto_atlixco/buscar_todas.php",
 			//data: "nombre=" . $('#txtnombre').val(),
 			error: function(){
 			 alert("no se puede conectar al servidor");	
@@ -30,9 +30,10 @@ document.addEventListener("deviceready",function(){
 	});//tap boton listado completo
 	
 	$('#btn_buscar').on('tap',function(){
+		alert($('#txtnombre').val());
 		$.ajax({
 			type: "POST",
-			url: "http://192.168.1.195/proyecto_atlixco/buscar_todas.php",
+			url: "http://192.168.1.195/proyecto_atlixco/busqueda_nombre.php",
 			data: "nombre=" . $('#txtnombre').val(),
 			error: function(){
 			 alert("no se puede conectar al servidor");	
