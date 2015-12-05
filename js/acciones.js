72f264595948368f21a58ba50740c44d2f934d63
AL03F13 <aponteeric95@gmail.com>
2015-12-05 11:29:02 -0600

    catorce

--- js/acciones.js
+++ js/acciones.js
@@ -4,7 +4,7 @@ document.addEventListener("deviceready",function(){
 	$('#btn_listar').on('tap',function(){
 		$.ajax({
 			type: "POST",
-			url: "http://192.168.1.195/proyecto_atlixco/buscar_todas.php",
+			url: "http://192.168.1.64/proyecto_atlixco/buscar_todas.php",
 			//data: "nombre=" . $('#txtnombre').val(),
 			error: function(){
 			 alert("no se puede conectar al servidor");	
@@ -20,7 +20,7 @@ document.addEventListener("deviceready",function(){
 				
 				for (var $x=0; $x<datos_t.tradiciones.length;$x++)
 				{
-				 	$('#contenido').append('<div align="center"><div style="background-color:#F33"><h4>'+ datos_t.tradiciones[$x].nombre +'</h4></div><div><h4>'+ datos_t.tradiciones[$x].fecha +'</h4></div><div><h4>'+ datos_t.tradiciones[$x].ubicacion +'</h4></div><div><h4>'+ datos_t.tradiciones[$x].descripcion +'</h4></div><div><img src="http://192.168.1.195/proyecto_atlixco/recursos/imagenes/fotos/'+ datos_t.tradiciones[$x].id_evento +'.jpg"></div></div>');
+				 	$('#contenido').append('<div align="center"><div><h4>'+ datos_t.tradiciones[$x].nombre +'</h4></div><div><h4>'+ datos_t.tradiciones[$x].fecha +'</h4></div><div><h4>'+ datos_t.tradiciones[$x].ubicacion +'</h4></div><div><h4>'+ datos_t.tradiciones[$x].descripcion +'</h4></div><div><img src="http://192.168.1.195/proyecto_atlixco/recursos/imagenes/fotos/'+ datos_t.tradiciones[$x].id_evento +'.jpg"></div></div>');
 				}
 				
 				
@@ -30,10 +30,10 @@ document.addEventListener("deviceready",function(){
 	});//tap boton listado completo
 	
 	$('#btn_buscar').on('tap',function(){
-		$('#txtnombre').val();
+		alert($('#txtnombre').val());
 		$.ajax({
 			type: "POST",
-			url: "http://192.168.1.195/proyecto_atlixco/busqueda_nombre.php",
+			url: "http://192.168.1.64/proyecto_atlixco/busqueda_nombre.php",
 			data: "nombre=" + $('#txtnombre').val(),
 			error: function(){
 			 alert("no se puede conectar al servidor");	
@@ -41,7 +41,7 @@ document.addEventListener("deviceready",function(){
 
 			success: function(respuesta)
 			{
-				
+				alert(respuesta);
 				var datos_t = JSON.parse(respuesta);
 				$('#busquedax').empty();
 				
@@ -49,7 +49,7 @@ document.addEventListener("deviceready",function(){
 				
 				for (var $x=0; $x<datos_t.tradiciones.length;$x++)
 				{
-				 	$('#busquedax').append('<div align="center"><div style="background-color:#F33"><h4>'+ datos_t.tradiciones[$x].nombre +'</h4></div><div><h4>'+ datos_t.tradiciones[$x].fecha +'</h4></div><div><h4>'+ datos_t.tradiciones[$x].ubicacion +'</h4></div><div><h4>'+ datos_t.tradiciones[$x].descripcion +'</h4></div><div><img src="http://192.168.1.195/proyecto_atlixco/recursos/imagenes/fotos/'+ datos_t.tradiciones[$x].id_evento +'.jpg"></div></div>');
+				 	$('#busquedax').append('<div align="center"><div><h4>'+ datos_t.tradiciones[$x].nombre +'</h4></div><div><h4>'+ datos_t.tradiciones[$x].fecha +'</h4></div><div><h4>'+ datos_t.tradiciones[$x].ubicacion +'</h4></div><div><h4>'+ datos_t.tradiciones[$x].descripcion +'</h4></div><div><img src="http://192.168.1.195/proyecto_atlixco/recursos/imagenes/fotos/'+ datos_t.tradiciones[$x].id_evento +'.jpg"></div></div>');
 				}
 				
 				
