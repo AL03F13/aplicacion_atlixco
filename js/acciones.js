3f0101796951bf870465b7fc65de9cd026156d67
AL03F13 <aponteeric95@gmail.com>
2015-12-05 09:33:22 -0600

    doce

--- js/acciones.js
+++ js/acciones.js
@@ -20,7 +20,7 @@ document.addEventListener("deviceready",function(){
 				
 				for (var $x=0; $x<datos_t.tradiciones.length;$x++)
 				{
-				 	$('#contenido').append('<div align="center"><div><h4>'+ datos_t.tradiciones[$x].nombre +'</h4></div><div><h4>'+ datos_t.tradiciones[$x].fecha +'</h4></div><div><h4>'+ datos_t.tradiciones[$x].ubicacion +'</h4></div><div><h4>'+ datos_t.tradiciones[$x].descripcion +'</h4></div><div><img src="http://192.168.1.195/proyecto_atlixco/recursos/imagenes/fotos/'+ datos_t.tradiciones[$x].id_evento +'.jpg"></div></div>');
+				 	$('#contenido').append('<div align="center"><div style="background-color:#F33"><h4>'+ datos_t.tradiciones[$x].nombre +'</h4></div><div><h4>'+ datos_t.tradiciones[$x].fecha +'</h4></div><div><h4>'+ datos_t.tradiciones[$x].ubicacion +'</h4></div><div><h4>'+ datos_t.tradiciones[$x].descripcion +'</h4></div><div><img src="http://192.168.1.195/proyecto_atlixco/recursos/imagenes/fotos/'+ datos_t.tradiciones[$x].id_evento +'.jpg"></div></div>');
 				}
 				
 				
@@ -49,7 +49,7 @@ document.addEventListener("deviceready",function(){
 				
 				for (var $x=0; $x<datos_t.tradiciones.length;$x++)
 				{
-				 	$('#busquedax').append('<div align="center"><div><h4>'+ datos_t.tradiciones[$x].nombre +'</h4></div><div><h4>'+ datos_t.tradiciones[$x].fecha +'</h4></div><div><h4>'+ datos_t.tradiciones[$x].ubicacion +'</h4></div><div><h4>'+ datos_t.tradiciones[$x].descripcion +'</h4></div><div><img src="http://192.168.1.195/proyecto_atlixco/recursos/imagenes/fotos/'+ datos_t.tradiciones[$x].id_evento +'.jpg"></div></div>');
+				 	$('#busquedax').append('<div align="center"><div style="background-color:#F33"><h4>'+ datos_t.tradiciones[$x].nombre +'</h4></div><div><h4>'+ datos_t.tradiciones[$x].fecha +'</h4></div><div><h4>'+ datos_t.tradiciones[$x].ubicacion +'</h4></div><div><h4>'+ datos_t.tradiciones[$x].descripcion +'</h4></div><div><img src="http://192.168.1.195/proyecto_atlixco/recursos/imagenes/fotos/'+ datos_t.tradiciones[$x].id_evento +'.jpg"></div></div>');
 				}
 				
 				
